b0f698e2a7952a6b31d516c8d1e99d7052e71370
Nathan Marley <nathan.marley@gmail.com>
2019-02-11 17:17:16 -0200

    use C char ptrs instead of GoString within C

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include "nautilus.h"
 #include <stdio.h>
 
+// for further info:
+// https://golang.org/cmd/cgo/#hdr-C_references_to_Go
+
 int main() {
+    char *name = "jack";
+
     printf("This is a C application\n");
-
-    // via https://golang.org/cmd/cgo/#hdr-C_references_to_Go :
-    //
-    // Go functions that take arguments of type string may be called with the C
-    // type _GoString_, described above. The _GoString_ type will be
-    // automatically defined in the preamble. Note that there is no way for C
-    // code to create a value of this type; this is only useful for passing
-    // string values from Go to C and back to Go.
-    GoString name = {"Jack", 4};
-
-    // ... because of the above, not sure how useful this really would ever be. :/
 
     SayHello(name);
     SayBye();
--- nautilus.go
+++ nautilus.go
@@ -4,16 +4,16 @@ import "C"
 import "fmt"
 
 //export SayHello
-func SayHello(name string) {
-    fmt.Printf("Nautilus says: Hello, %s!\n", name)
+func SayHello(name *C.char) {
+	fmt.Printf("Nautilus says: Hello, %s!\n", C.GoString(name))
 }
 
 //export SayBye
 func SayBye() {
-    fmt.Println("Nautilus says: Bye!")
+	fmt.Println("Nautilus says: Bye!")
 }
 
 func main() {
-    // We need the main function to enable
-    // CGO compiler to compile the package as a C library.
+	// We need the main function to enable
+	// CGO compiler to compile the package as a C library.
 }
